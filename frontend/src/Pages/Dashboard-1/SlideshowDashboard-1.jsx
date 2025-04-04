fd73533fa49918b28c195714e9cdf147a64b39ad
mukesh samardha <mukeshsamardhaginne@gmail.com>
2025-04-04 23:35:42 +0530

    frontend changes

--- frontend/src/Pages/Dashboard-1/SlideshowDashboard-1.jsx
+++ frontend/src/Pages/Dashboard-1/SlideshowDashboard-1.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './SlideshowDashboard-1.css';
+
 const images = [
   "https://png.pngtree.com/png-clipart/20230427/original/pngtree-productivity-line-icon-png-image_9116967.png",
   "https://png.pngtree.com/png-vector/20220718/ourmid/pngtree-quarantine-office-people-employee-manager-png-image_5913171.png",
@@ -9,104 +10,71 @@ const images = [
 ];
 
 function SlideShow() {
-    const [index, setIndex] = useState(0);
-    const [img, setImg] = useState(images[index]);
-    function current_slide(i) {
-        setIndex(i);
-        setImg(images[i]);
-    }
-    async function slide_next(i){
-        if (i == 3) {
-            let k = 0;
-            setIndex(k);
-            setImg(images[k]);
-        } else {
-            let k = i + 1;
-            setIndex(k);
-            setImg(images[k]);
-        }
-    }
-   async function slide_prev(i) {
-        if (i == 0) {
-            let k = 3;
-            setIndex(k);
-            setImg(images[k]);
-        } else {
-            let k = i - 1;
-            setIndex(k);
-            setImg(images[k]);
-        }
-    }
-    function slide_change() {
-        // setInterval(async() => {await slide_next(index)}, 1000);
-        //doubt
-    }
-    slide_change();
-    return (
-      <section id="slideshowsection">
-        <div id="slideshowmain">
-          <div id="slideshowmaindiv">
-            <button
-              className="slideshowbtnpn"
-              onClick={() => {
-                slide_prev(index);
-              }}
-            >
-              Prev
-            </button>
-            <div id="slideshowimgdiv">
-              <img src={img} alt="NO Image" id="slideshowimg"></img>
-            </div>
-            <button
-              className="slideshowbtnpn"
-              onClick={() => {
-                slide_next(index);
-              }}
-            >
-              Next
-            </button>
-          </div>
-          <div id="slideshowbtndiv">
-            <button
-              onClick={() => {
-                current_slide(0);
-              }}
-            ></button>
-            <button
-              onClick={() => {
-                current_slide(1);
-              }}
-            ></button>
-            <button
-              onClick={() => {
-                current_slide(2);
-              }}
-            ></button>
-            <button
-              onClick={() => {
-                current_slide(3);
-              }}
-            ></button>
-          </div>
-        </div>
-        <div id="slideshowsidebtn">
-          <h3>Choose Your Role!</h3>
-          <button>
-            <img src="/img/admin.png"></img>
-            Admin
-          </button>
-          <button>
-            <img src="/img/manager.png"></img>Manager
-          </button>
-          <button>
-            <img src="/img/employee.png"></img>
-            Employee
-          </button>
-        </div>
-      </section>
+  const [index, setIndex] = useState(0);
+
+  function current_slide(i) {
+    setIndex(i);
+  }
+
+  function slide_next() {
+    setIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
+  }
+
+  function slide_prev() {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  }
+
+  useEffect(() => {
+    const interval = setInterval(slide_next, 3000);
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+
+  return (
+    <section id="slideshowsection">
+      <div id="slideshowmain">
+        <div id="slideshowmaindiv">
+          <button className="slideshowbtnpn" onClick={slide_prev}>
+            Prev
+          </button>
+          <div id="slideshowimgdiv">
+            <img src={images[index]} alt="Slideshow" id="slideshowimg" />
+          </div>
+          <button className="slideshowbtnpn" onClick={slide_next}>
+            Next
+          </button>
+        </div>
+        <div id="slideshowbtndiv">
+          {images.map((_, i) => (
+            <button key={i} onClick={() => current_slide(i)
+            }></button>
+          ))}
+        </div>
+      </div>
+      <div id="slideshowsidebtn">
+        <h2>Choose Your Role!</h2>
+        <button>
+          <img src="/img/admin.png" alt="Admin" />
+          Admin
+        </button>
+        <button>
+          <img src="/img/manager.png" alt="Manager" />
+          Manager
+        </button>
+        <button>
+          <img src="/img/employee.png" alt="Employee" />
+          Employee
+        </button>
+      </div>
+    </section>
+  );
 }
-
-
-
+const styles = {
+  hi: {
+    backgroundColor: "black"
+  }
+}
 export default SlideShow;
